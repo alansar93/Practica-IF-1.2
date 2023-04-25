7ae1a10266586a8512bb76ef2a97fed34974e2c9
Alan  Sarchman <alansarhchman@live.com.argit config --global user.emailgit config --globalgit config --global config user.namegit initgit config --global user.email alansarhchman@live.com.ar>
2023-04-24 21:09:47 -0300

    modificado

--- ej2.js
+++ ej2.js
@@ -1,14 +1,28 @@
 const BITCOIN = 1;
 const BITCOIN_A_ETHEREUM = 13540;
 const BITCOIN_A_DOLAR = 39685.40;
+ 
+const tipoCambio = document.querySelector ("#usd")
+const cantidadBitcoin = document.querySelector ("#eth")
+const pEtherium = document.querySelector("#ethereum")
+const pDolar = document.querySelector ("#dolar")
 
-let cantidadBitcoin = prompt ("ingrese cuantos bitcoin va a convertir");
+// let cantidadBitcoin = prompt ("ingrese cuantos bitcoin va a convertir");
 let ethereum = "ethereum";
 let dolar = "dolar";
-let tipoCambio = prompt ("Dolar o Ethereum");
+// let tipoCambio = prompt ("Dolar o Ethereum");
+ let btcRecibido = "def"; 
 
+
+function cambioEth () {
+    btcRecibido = cantidadBitcoin.value;
+    console.log (btcRecibido)
+}
+/* 
 if ((tipoCambio == ethereum) && (cantidadBitcoin > "0")) {
-    document.write ("Usted tiene " + (BITCOIN_A_ETHEREUM * cantidadBitcoin) + " ethereum");
-} else if ((tipoCambio == dolar) && (cantidadBitcoin > "0")) {
-    document.write ("Usted tiene " + (BITCOIN_A_DOLAR * cantidadBitcoin) + " dolar");
-}else (document.write ("No se puede convertir"))
+    /* document.write ("Usted tiene " + (BITCOIN_A_ETHEREUM * cantidadBitcoin) + " ethereum"); */
+/* pEtherium.innerHTML = ("Usted recibiria " + (cantidadBitcoin * BITCOIN_A_ETHEREUM))
+} else if ((tipoCambio == dolar) && (cantidadBitcoin > "0")) { */
+    /* document.write ("Usted tiene " + (BITCOIN_A_DOLAR * cantidadBitcoin) + " dolar") */
+/* }  */ /* (document.write ("No se puede convertir") *//* ) */
+ /* */ 
